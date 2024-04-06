eacb845f195d94b3953505a682f31b293afbe537
atalya84 <51419032+atalya84@users.noreply.github.com>
2024-04-06 21:51:15 +0300

    add USER interface and scheme

---
+++ src/models/user_model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    _id?: string;
+    email: string;
+    password: string;
+    name: string;
+    lastName: string;
+    imgUrl?: string;
+    refreshTokens?: string[];
+    // comments: Array<{ type: mongoose.Schema.Types.ObjectId; ref: "comments" }>;
+    // items: Array<{ type: mongoose.Schema.Types.ObjectId; ref: "Items"}>
+  }
+  
+  const userSchema = new mongoose.Schema<IUser>({
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: false
+    },
+    lastName: {
+        type: String,
+        required: false
+      },
+    password: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+    },
+    // items: {
+    //   type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Items" }]
+    // },
+    refreshTokens: {
+      type: [String],
+      required: false,
+    }
+  });
+
+  export default mongoose.model<IUser>("User", userSchema);
