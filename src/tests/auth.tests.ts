07024f994f4bc42b19058b304fc45cb330ef6d8f
Guy Moore <guymoore8@gmail.com>
2024-04-15 22:45:16 +0300

    fixed build

--- src/tests/auth.tests.ts
+++ src/tests/auth.tests.ts
@@ -1,18 +1,17 @@
 import { Express } from "express";
+import initApp from "../app";
 
 let app: Express;
 
 const user = {
-    email: "jhonDoe@test.com",
-    password: "abcd1234",
-    name: "jhon"
-}
+  email: "jhonDoe@test.com",
+  password: "abcd1234",
+  name: "jhon",
+};
 
 beforeAll(async () => {
-    app = await initApp();
-    console.log("beforeAll");
+  app = await initApp();
+  console.log("beforeAll");
 });
 
-describe("Auth tests", () => {
-
-})
+describe("Auth tests", () => {});
