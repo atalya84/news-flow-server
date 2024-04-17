c91388422139a4ba5f1943e98be1fca80c640640
atalya84 <51419032+atalya84@users.noreply.github.com>
2024-04-17 19:31:57 +0300

    use https for server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import initApp from "./app";
+import https from 'https';
 
 initApp().then((app) => {
-  app.listen(process.env.PORT, () => {
-    console.log(
-      "App is listening at http://localhost:" + process.env.PORT || 3000
-    );
-  });
+  https.createServer(app).listen(process.env.PORT, () => {
+      console.log(
+        "App is listening at http://localhost:" + process.env.PORT || 3000
+      );
+    });
 });
