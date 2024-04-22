52cd0c14b2d55eef3013a40f8775708e041684d2
atalya84 <51419032+atalya84@users.noreply.github.com>
2024-04-22 18:26:26 +0300

    add register function

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -3,6 +3,7 @@ import { OAuth2Client } from 'google-auth-library';
 import User, { IUser } from '../models/user_model';
 import jwt from 'jsonwebtoken';
 import { Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const client = new OAuth2Client();
 
@@ -35,6 +36,38 @@ export const googleSignin = async (req: Request, res: Response) => {
         return res.status(400).send(err.message);
     }
 
+}
+
+export const register = async (req: Request, res: Response) => {
+    const email = req.body?.email;
+    const password = req.body?.password;
+    const imgUrl = req.body?.imgUrl;
+    const name = req.body?.name;
+    if (!email || !password) {
+        return res.status(400).send("missing email or password");
+    }
+    try {
+        const rs = await User.findOne({ 'email': email });
+    if (rs != null) {
+        return res.status(406).send("email already exists");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(password, salt);
+    const rs2 = await User.create({
+        'email': email,
+        'password': encryptedPassword,
+        'imgUrl': imgUrl,
+        'name': name
+    });
+    const tokens = await generateTokens(rs2)
+    const added_user = await User.findOne({"email": email});
+    res.status(201).send({
+        user: added_user,
+        ...tokens
+    })
+    } catch (err) {
+        return res.status(400).send("error missing email or password");
+    }
 }
 
 const generateTokens = async (user: Document & IUser) => {
