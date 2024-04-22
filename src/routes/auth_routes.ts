cbebab1f1511b2fea1f39f9c1ca88baf8ca2c4c8
atalya84 <51419032+atalya84@users.noreply.github.com>
2024-04-22 18:27:07 +0300

    add register route

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -1,7 +1,30 @@
 import express from "express";
 const router = express.Router();
-import { googleSignin } from "../controllers/auth_controller";
+import { googleSignin, register } from "../controllers/auth_controller";
+import { upload } from "../common/file_upload";
 
 router.post("/google", googleSignin);
+
+/**
+* @swagger
+* /auth/register:
+*   post:
+*     summary: registers a new user
+*     tags: [Auth]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/User'
+*     responses:
+*       200:
+*         description: The new user
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/User'
+*/
+router.post("/register", upload.array("image"), register);
 
 export default router;
