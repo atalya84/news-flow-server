2fd0f2baac8971f200bd99bc9f79d312b86d3032
Guy Moore <guymoore8@gmail.com>
2024-05-10 18:24:12 +0300

    modified post model

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,19 +1,18 @@
 import { Schema, model } from 'mongoose';
-import { IComment } from './comment.model';
-import { commentSchema } from './comment.model';
 
 export interface IPost {
+	_id: string;
 	title: string;
 	image: string;
-	comments: IComment[];
-	owner: string;
+	userId: string;
+	created: Date;
 }
 
 const postSchema = new Schema<IPost>({
 	title: { type: String, required: true },
-	owner: { type: String, required: true },
-	comments: [commentSchema],
+	userId: { type: String, required: true },
 	image: String,
+	created: { type: Date, required: true },
 });
 
 export default model<IPost>('Post', postSchema);
