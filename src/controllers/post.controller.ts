b7d9659de96b8f42b8f004aa811db50ce594d414
Guy Moore <guymoore8@gmail.com>
2024-05-10 18:25:08 +0300

    added functions to fetch posts by user or by date range

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -15,13 +15,36 @@ class PostController extends BaseController<IPost> {
 		super.get(req, res);
 	}
 	async post(req: Request, res: Response): Promise<void> {
+		req.body.created = new Date();
 		super.post(req, res);
 	}
 	async put(req: Request, res: Response): Promise<void> {
 		super.put(req, res);
 	}
 	async delete(req: Request, res: Response): Promise<void> {
-		super.post(req, res);
+		super.delete(req, res);
+	}
+	async getPostsByUserId(req: Request, res: Response): Promise<void> {
+		try {
+			const userId: string = req.params.id;
+			const userPosts: IPost[] = await postModel.find({ userId });
+			res.status(200).json(userPosts);
+		} catch (err) {
+			console.error('Error: ', err.message);
+			res.status(500).json({ message: err.message });
+		}
+	}
+	async getPostsByDate(req: Request, res: Response): Promise<void> {
+		try {
+			const range: Date = new Date(req.params.date);
+			const posts: IPost[] = await postModel.find({
+				created: { $gte: range },
+			});
+			res.status(200).json(posts);
+		} catch (err) {
+			console.error('Error: ', err.message);
+			res.status(500).json({ message: err.message });
+		}
 	}
 }
 
