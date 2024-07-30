2133aeaaa5b0af3fd341ca28de66c6acdc2c29a6
atalya84 <atalyanagar@gmail.com>
2024-07-30 22:02:04 +0300

    add production https

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
 import initApp from "./app";
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 initApp().then((app) => {
-  app.listen(process.env.PORT, () => {
-    console.log(
-      "App is listening at http://localhost:" + process.env.PORT || 3000
-    );
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Development mode');
+    http.createServer(app).listen(process.env.PORT, () => {
+      console.log(
+        "App is listening at http://localhost:" + process.env.PORT || 3000
+      );
+    });
+  } else {
+    console.log('Production mode');
+    const options = {
+      key: fs.readFileSync('./client-key.pem'),
+      cert: fs.readFileSync('./client-cert.pem')
+    };
+    https.createServer(options, app).listen(process.env.HTTPS_PORT, () => {
+      console.log(
+        "App is listening at https://localhost:" + process.env.HTTPS_PORT
+      );
+    });
+  }
 });
